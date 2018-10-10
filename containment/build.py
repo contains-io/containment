6212368b65f00f28bd326f5e30e5cc80a2db91e8
Melissa Nuño <dangle@contains.io>
2018-10-10 14:43:32 -0600

    Add precommit hooks.

--- containment/build.py
+++ containment/build.py
@@ -1,7 +1,8 @@
-#from .types import BuildID
+# -*- coding: utf-8 -*-
+"""If the dockerfile exists then do not build, because the file is only written at the end of a successful build. The name of the file is the docker image id. It will be used as the argument to 'docker run'."""
+# from .types import BuildID
 import docker
 
-"""If the dockerfile exists then do not build, because the file is only written at the end of a successful build. The name of the file is the docker image id. It will be used as the argument to 'docker run'."""
 
 def _combine(community, base):
     return df_contents
--- containment/test_cli/test_activate.py
+++ containment/test_cli/test_activate.py
@@ -2,6 +2,7 @@
 """Activation tests for contain."""
 from ..cli import COMMUNITY_OS_LAYER
 
+
 def test_activate():
     """Test activation under ideal conditions."""
     pass
