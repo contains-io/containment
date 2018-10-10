5342bad9fd4b26dd9103c22b9ee4253d396aaea7
Melissa Nuño <dangle@contains.io>
2018-10-10 11:49:16 -0600

    Add support for zsh and format with black.

--- containment/cli.py
+++ containment/cli.py
@@ -35,7 +35,12 @@ class Profile:
         self.home = os.environ["HOME"]
         self.path = pathlib.Path(self.home).joinpath(".containment")
         self.projects = self.path.joinpath("projects")
-        self.package_list = ["vim", "tmux", "git"] # These are examples.
+        self.package_list = [
+            "vim",
+            "tmux",
+            "git",
+            os.path.split(os.environ["SHELL"])[1],
+        ]  # These are examples.
         self.os_packages = self.path.joinpath("os_packages.json")
         self.lang_packages = self.path.joinpath("lang_packages.json")
 
@@ -51,7 +56,7 @@ class Project:
         self.entrypoint = self.path.joinpath("entrypoint.sh")
         self.os_packages = self.path.joinpath("os_packages.json")
         self.lang_packages = self.path.joinpath("lang_packages.json")
-        
+
 
 class Context:
     def __init__(self, cli):
@@ -59,30 +64,31 @@ class Context:
         self.user = os.environ["USER"]
         self.shell = os.environ["SHELL"]
         self.userid = subprocess.getoutput("id -u")
-        self.dockergid =\
-            subprocess.getoutput("grep docker /etc/group").split(':')[2]
+        self.dockergid = subprocess.getoutput("grep docker /etc/group").split(
+            ":"
+        )[2]
         # CONFIGURATION STRINGS
-        self.project_adapter = \
-        f"""RUN     useradd -G docker --uid {self.userid} --home /home/{self.user} {self.user}
+        self.project_adapter = f"""RUN     useradd -G docker --uid {self.userid} --home /home/{self.user} {self.user}
         RUN     echo {self.user} ALL=\(ALL\) NOPASSWD: ALL >> /etc/sudoers
         COPY    ./entrypoint.sh entrypoint.sh
         RUN     chmod +x entrypoint.sh"""
-        self.entrypoint_text = f"""#! {self.shell}
+        self.entrypoint_text = f"""#!{self.shell}
         cd {cli.community.pathname}
         sudo sed -ie s/docker:x:[0-9]*:{self.user}/docker:x:{self.dockergid}:{self.user}/g /etc/group
+        sudo usermod -s {self.shell} {self.user}
         exec {self.shell}"""
-        self.run_text = \
-        f"""docker run -it \
+        self.run_text = f"""docker run -it \
                        -v /var/run/docker.sock:/var/run/docker.sock \
                        -v {cli.profile.home}:{cli.profile.home} \
                        -v {cli.community.pathname}:{cli.community.pathname} \
                        --entrypoint=/entrypoint.sh -u {self.user}:{self.dockergid} {cli.project.tag}:latest"""
 
-
-        self.externalbasis = ("ubuntu@sha256:c8c275751219dadad8fa56b3ac41ca6cb22219ff117ca9"
-                         "8fe82b42f24e1ba64e")
+        self.externalbasis = (
+            "ubuntu@sha256:c8c275751219dadad8fa56b3ac41ca6cb22219ff117ca9"
+            "8fe82b42f24e1ba64e"
+        )
         self.base_text = f"""FROM    {self.externalbasis}
-        RUN     apt-get update && apt-get install -y sudo docker.io"""
+        RUN     apt update && apt install -y sudo docker.io"""
 
 
 class Command_Line_Interface:
@@ -92,11 +98,12 @@ class Command_Line_Interface:
         self.profile = Profile()
         self.project = Project(self)
         self.context = Context(self)
-        self.pkg_install_cmds = {"debian":   "apt-get install -y",
-                                 "ubuntu":   "apt-get install -y",
-                                 "python3":  "`which pip3` install",
-                                 "python":   "`which pip` install"}
-
+        self.pkg_install_cmds = {
+            "debian": "apt install -y",
+            "ubuntu": "apt install -y",
+            "python3": "`which pip3` install",
+            "python": "`which pip` install",
+        }
 
     def _os_install(self, package_file):
         """
@@ -104,32 +111,31 @@ class Command_Line_Interface:
         one RUN per package type
         one package type per JSON key
         """
-        packages = ' '.join(json.load(package_file.open()))
+        packages = " ".join(json.load(package_file.open()))
         if packages:
             for packager in self.pkg_install_cmds:
                 if packager in self.context.externalbasis:
                     installer = self.pkg_install_cmds[packager]
-            return f'RUN    {installer} {packages}'
+            return f"RUN    {installer} {packages}"
         else:
-            return ''
-
+            return ""
 
     def _lang_install(self, package_file):
         """
         """
         packages_dict = json.load(package_file.open())
-        install_command = ''
+        install_command = ""
         if packages_dict:
             for lang in packages_dict:
                 if lang in self.pkg_install_cmds:
-                    packages = ' '.join(packages_dict[lang])
+                    packages = " ".join(packages_dict[lang])
                     installer = self.pkg_install_cmds[lang]
-                    install_command =\
-                        install_command + f'RUN    {installer} {packages}\n'
+                    install_command = (
+                        install_command + f"RUN    {installer} {packages}\n"
+                    )
             return install_command
         else:
-            return ''
-
+            return ""
 
     def pave_profile(self):
         """
@@ -138,15 +144,10 @@ class Command_Line_Interface:
         """
         self.profile.path.mkdir()
         json.dump(
-            self.profile.package_list,
-            self.profile.os_packages.open(mode='w')
+            self.profile.package_list, self.profile.os_packages.open(mode="w")
         )
-        json.dump(
-            {},
-            self.profile.lang_packages.open(mode='w')
-        )
+        json.dump({}, self.profile.lang_packages.open(mode="w"))
         self.profile.projects.mkdir()
-
 
     def pave_project(self):
         """
@@ -160,7 +161,6 @@ class Command_Line_Interface:
         self.project.lang_packages.write_text("{}")
         self.write_dockerfile()
 
-
     def pave_community(self):
         """
         Usage:
@@ -171,7 +171,6 @@ class Command_Line_Interface:
         self.community.os_packages.write_text("[]")
         self.community.lang_packages.write_text("{}")
 
-
     def _assure_config(self):
         if not self.community.containment.is_dir():
             self.pave_community()
@@ -180,25 +179,26 @@ class Command_Line_Interface:
         if not self.project.path.is_dir():
             self.pave_project()
 
-
     def write_dockerfile(self):
         """
         Usage:
-          containment write_dockerfile  
+          containment write_dockerfile
         """
         self.project.dockerfile.write_text(self._assemble_dockerfile())
 
     def _assemble_dockerfile(self):
-        return '\n'.join(
-            [self.community.base.read_text(),
-             self._os_install(self.community.os_packages),
-             self._os_install(self.profile.os_packages),
-             self._os_install(self.project.os_packages),
-             self._lang_install(self.community.lang_packages),
-             self._lang_install(self.profile.lang_packages),
-             self._lang_install(self.project.lang_packages),
-             self.context.project_adapter]
-            )
+        return "\n".join(
+            [
+                self.community.base.read_text(),
+                self._os_install(self.community.os_packages),
+                self._os_install(self.profile.os_packages),
+                self._os_install(self.project.os_packages),
+                self._lang_install(self.community.lang_packages),
+                self._lang_install(self.profile.lang_packages),
+                self._lang_install(self.project.lang_packages),
+                self.context.project_adapter,
+            ]
+        )
 
     def build(self):
         """
@@ -218,11 +218,10 @@ class Command_Line_Interface:
         run_command = self.project.runfile.read_text().split()
         chmod_string = "chmod +x " + self.project.runfile.absolute().as_posix()
         chmod_run = subprocess.run(chmod_string.split())
-        run_subprocess = subprocess.run(run_command,
-                                        stdin=sys.stdin,
-                                        stdout=sys.stdout,
-                                        stderr=sys.stderr)
-        
+        run_subprocess = subprocess.run(
+            run_command, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr
+        )
+
 
 def activate():
     """
@@ -231,7 +230,7 @@ def activate():
     """
     # This is derived from the clone
     cli = Command_Line_Interface()
-    cli._assure_config() 
+    cli._assure_config()
     cli.write_dockerfile()
     cli.build()
     cli.run()
